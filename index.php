8dd458b482a7b489bd1bf6f0b1a21aa968569076
rrdmp15 <rdayismar@gmail.com>
2023-06-27 17:03:19 -0500

    feat: :sparkles: include y Require
    Ejemplos de las funciones include, require, include_once y require_once en PHP, destacando su utilidad para incluir archivos externos y modularizar el código. Estas funciones son fundamentales para la reutilización y organización eficiente del código en un programa PHP

--- index.php
+++ index.php
@@ -1,28 +1,11 @@
 <?php
-// Ejemplo de función definida por el usuario:
-function saludar($nombre) {
-    echo "¡Hola, $nombre!";
-}
-
-saludar("Juan");
-
-// Ejemplo de función en PHP:
-echo strlen("Hola");
-
-// Ejemplo de función que retorna un valor:
-function sumar($num1, $num2) {
-    return $num1 + $num2;
-}
-
-$resultado = sumar(3, 5);
-echo "El resultado de la suma es: $resultado";
-
-// Ejemplo de función que no retorna un valor:
-function imprimirFecha() {
-    echo date("d/m/Y");
-}
-
-echo "La fecha de hoy es: ";
-imprimirFecha();
+// Ejemplo de include:
+include "archivo.php";
+// Ejemplo de require:
+require "archivo.php";
+// Ejemplo de include_once:
+include_once "archivo.php";
+// Ejemplo de require_once:
+require_once "archivo.php";
 ?>
 
