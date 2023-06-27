ef9b7cebb371f4a71aec4e76a047ad9bf1c0d9fd
rrdmp15 <rdayismar@gmail.com>
2023-06-27 17:47:11 -0500

    feat: :sparkles: Interfaces
    En este ejemplo, se muestra el uso de interfaces en PHP para establecer contratos entre clases. La interfaz define métodos que deben ser implementados por las clases que la utilizan, asegurando consistencia en la funcionalidad.

--- index.php
+++ index.php
@@ -1,30 +1,26 @@
 <?php
 
-abstract class Figura {
-    protected $color;
-  
-    public function __construct($color) {
-      $this->color = $color;
-    }
-  
-    abstract public function calcularArea();
+interface Animal {
+    public function hacerSonido();
   }
   
-  class Cuadrado extends Figura {
-    private $lado;
-  
-    public function __construct($color, $lado) {
-      parent::__construct($color);
-      $this->lado = $lado;
-    }
-  
-    public function calcularArea() {
-      return $this->lado * $this->lado;
+  class Perro implements Animal {
+    public function hacerSonido() {
+      echo "Woof!";
     }
   }
   
-  $cuadrado = new Cuadrado("rojo", 5);
-  echo "El área del cuadrado es: " . $cuadrado->calcularArea();
+  class Gato implements Animal {
+    public function hacerSonido() {
+      echo "Meow!";
+    }
+  }
+  
+  $perro = new Perro();
+  $gato = new Gato();
+  
+  $perro->hacerSonido(); 
+  $gato->hacerSonido(); 
   
 ?>
 
