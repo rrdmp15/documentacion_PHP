f69a31d3a1391722219c656de3e3380543bd8bb0
rrdmp15 <rdayismar@gmail.com>
2023-06-27 16:52:24 -0500

    feat: :sparkles: Isset() y Empty()
    Ejemplos de las funciones isset() y empty() en PHP. Estas funciones proveen una manera sencilla de verificar si una variable está definida y no es nula, o si está vacía, permitiendo una lógica más robusta y eficiente en la programación PHP.

--- index.php
+++ index.php
@@ -1,44 +1,21 @@
 <?php
-// Array
-$frutas = array("Manzana", "Banana", "Naranja");
-echo "array de frutas: ";
-print_r($frutas);
+// isset()
+$nombre = "Juan";
 
-// Acceder a un elemento del array
-echo "Elemento 0: " . $frutas[0];
+if (isset($nombre)) {
+    echo "La variable 'nombre' está definida y no es nula.";
+} else {
+    echo "La variable 'nombre' no está definida o es nula.";
+}
 
-// array asociativo
-$edades = array("Juan" => 25, "María" => 30, "Pedro" => 35);
-echo "array asociativo de edades: ";
-print_r($edades);
+// empty()
+$edad = 30;
 
-// Acceder a un elemento del array asociativo
-echo "Edad de María: " . $edades["María"];
+if (empty($edad)) {
+    echo "La variable 'edad' está vacía o es cero.";
+} else {
+    echo "La variable 'edad' no está vacía y tiene un valor asignado.";
+}
 
-// Funciones para arrays
-$numeros = array(1, 2, 3, 4, 5);
-
-// Contar elementos en el array
-$conteo = count($numeros);
-echo "Número de elementos: " . $conteo;
-
-// Ordenar el array
-sort($numeros);
-echo "array ordenado: ";
-print_r($numeros);
-
-// Verificar si un elemento existe en el array
-$existe = in_array(3, $numeros);
-echo "¿El número 3 existe en el array? " . ($existe ? "Sí" : "No");
-
-// Obtener las claves de un array asociativo
-$claves = array_keys($edades);
-echo "Claves del array asociativo de edades: ";
-print_r($claves);
-
-// Obtener los valores de un array asociativo
-$valores = array_values($edades);
-echo "Valores del array asociativo de edades: ";
-print_r($valores);
 ?>
 
