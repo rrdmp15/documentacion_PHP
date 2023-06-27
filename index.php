b9796af416d7ef1dd808be26f08eb05e3e28d132
rrdmp15 <rdayismar@gmail.com>
2023-06-27 16:45:19 -0500

    feat: :sparkles: Operadores
    Ejemplos de operadores en PHP: aritméticos, asignación, arrays, comparación, lógicos, bitwise, ejecución, control de errores e incremento/decremento. Aportan claridad y utilidad en la comprensión y aplicación de los operadores en el desarrollo PHP.

--- index.php
+++ index.php
@@ -1,17 +1,79 @@
 <?php
-$entero = 10;
-$puntoFlotante = 3.14;
-$cadena = "Hola, mundo!";
-$booleano = true;
-$arreglo = [1, 2, 3];
-$objeto = new stdClass();
-$nulo = null;
+// operadores aritmeticos
 
-var_dump($entero);
-var_dump($puntoFlotante);
-var_dump($cadena);
-var_dump($booleano);
-var_dump($arreglo);
-var_dump($objeto);
-var_dump($nulo);
+$a = 10;
+$b = 5;
+
+$suma = $a + $b;
+$resta = $a - $b;
+$multiplicacion = $a * $b;
+$division = $a / $b;
+$modulo = $a % $b;
+
+// operadores de asignacion
+
+$a = 10;
+$b = 5;
+
+$a += $b; 
+$a -= $b; 
+$a *= $b;
+$a /= $b; 
+$a %= $b;
+
+// operadores de arrays
+
+$array1 = [1, 2, 3];
+$array2 = [4, 5, 6];
+
+$concatenacion = $array1 + $array2; 
+
+// operadores de comparacion
+
+$a = 10;
+$b = 5;
+
+$igual = ($a == $b); 
+$diferente = ($a != $b); 
+$mayor = ($a > $b); 
+$menor = ($a < $b); 
+$mayorOigual = ($a >= $b); 
+$menorOigual = ($a <= $b); 
+
+// operadores logicos
+
+$a = true;
+$b = false;
+
+$and = ($a && $b); 
+$or = ($a || $b); 
+$not = !$a; 
+
+// operadores bitwise
+
+$a = 5;
+$b = 3;
+
+$andBitwise = $a & $b;
+$orBitwise = $a | $b;
+$xorBitwise = $a ^ $b;
+$desplazamientoIzquierda = $a << $b; 
+$desplazamientoDerecha = $a >> $b;
+
+
+// operador de ejecucion
+
+$resultado = `ls -la`; 
+
+// operador de control de errores
+
+$resultado = @file_get_contents("archivo.txt");
+
+// operador de incremento y decremento
+
+$a = 5;
+
+$incremento = ++$a; 
+$decremento = --$a; 
+
 ?>
