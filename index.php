f1f58a8f7db06f2289c82f69d40877339cc4a537
rrdmp15 <rdayismar@gmail.com>
2023-06-27 17:12:10 -0500

    feat: :sparkles: Modificadores de acceso
    Ejemplos prácticos en PHP sobre los modificadores de acceso (public, private, protected) para un commit, enfocados en asegurar la correcta encapsulación y restricción de acceso a los miembros de una clase, mejorando la estructura y seguridad del código.

--- index.php
+++ index.php
@@ -1,51 +1,22 @@
 <?php
-// Ejemplo de Clase y Objeto:
-class Persona {
-  public $nombre;
-  public $edad;
-
-  public function saludar() {
-    echo "¡Hola, mi nombre es " . $this->nombre . " y tengo " . $this->edad . " años!";
+class MiClase {
+    public $publico = "Este es un miembro público";
+    private $privado = "Este es un miembro privado";
+    protected $protegido = "Este es un miembro protegido";
+  
+    public function obtenerPrivado() {
+      return $this->privado;
+    }
+  
+    protected function obtenerProtegido() {
+      return $this->protegido;
+    }
   }
-}
-
-$persona = new Persona();
-$persona->nombre = "Juan";
-$persona->edad = 25;
-$persona->saludar();
-
-// Ejemplo de Atributos y Métodos:
-class Circulo {
-  public $radio;
-
-  public function calcularArea() {
-    return pi() * pow($this->radio, 2);
-  }
-}
-
-$circulo = new Circulo();
-$circulo->radio = 5;
-$area = $circulo->calcularArea();
-echo "El área del círculo es: " . $area;
-
-// Ejemplo de Herencia:
-class Animal {
-  public $nombre;
-
-  public function saludar() {
-    echo "¡Hola, soy un animal llamado " . $this->nombre . "!";
-  }
-}
-
-class Perro extends Animal {
-  public function ladrar() {
-    echo "¡Guau guau!";
-  }
-}
-
-$perro = new Perro();
-$perro->nombre = "Max";
-$perro->saludar();
-$perro->ladrar();
+  
+  $instancia = new MiClase();
+  echo $instancia->publico;
+  echo $instancia->obtenerPrivado();
+  echo $instancia->obtenerProtegido();
+  
 ?>
 
