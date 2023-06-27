71555d863f2613564158ebc38d67178fc033bcec
rrdmp15 <rdayismar@gmail.com>
2023-06-27 17:05:35 -0500

    feat: :sparkles: json_encode y json_decode
    Ejemplos de las funciones json_encode y json_decode en PHP para la manipulación de datos en formato JSON. Estas funciones posibilitan la codificación y decodificación de datos, facilitando el intercambio y procesamiento de información en aplicaciones web y servicios API

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
-// Ejemplo de include:
-include "archivo.php";
-// Ejemplo de require:
-require "archivo.php";
-// Ejemplo de include_once:
-include_once "archivo.php";
-// Ejemplo de require_once:
-require_once "archivo.php";
+// Ejemplo de json_encode:
+$data = array(
+    'name' => 'John Doe',
+    'age' => 30,
+    'email' => 'johndoe@example.com'
+);
+
+$json = json_encode($data);
+
+echo $json;
+
+// Ejemplo de json_decode:
+$json = '{"name":"John Doe","age":30,"email":"johndoe@example.com"}';
+
+$data = json_decode($json);
+
+echo "Name: " . $data->name . "<br>";
+echo "Age: " . $data->age . "<br>";
+echo "Email: " . $data->email . "<br>";
 ?>
 
