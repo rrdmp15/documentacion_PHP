df21954541556f7d7e054f35a7c9d3523763655f
rrdmp15 <rdayismar@gmail.com>
2023-06-27 16:56:03 -0500

    feat: :sparkles: Estructuras de control
    Ejemplos de estructuras de control condicionales (if, switch) y de repetición (for, while, do-while) en PHP. Contribuyen a comprender y aplicar las distintas estructuras para tomar decisiones y repetir acciones según las condiciones establecidas en un programa PHP.

--- index.php
+++ index.php
@@ -1,21 +1,46 @@
 <?php
-// isset()
-$nombre = "Juan";
-
-if (isset($nombre)) {
-    echo "La variable 'nombre' está definida y no es nula.";
-} else {
-    echo "La variable 'nombre' no está definida o es nula.";
-}
-
-// empty()
-$edad = 30;
-
-if (empty($edad)) {
-    echo "La variable 'edad' está vacía o es cero.";
-} else {
-    echo "La variable 'edad' no está vacía y tiene un valor asignado.";
-}
-
+    // Estructura de control condicional "if":
+    $edad = 25;
+    
+    if ($edad >= 18) {
+        echo "Eres mayor de edad.";
+    } else {
+        echo "Eres menor de edad.";
+    }
+    // Estructura de control condicional "switch":
+    $diaSemana = "Martes";
+    
+    switch ($diaSemana) {
+        case "Lunes":
+            echo "Hoy es lunes.";
+            break;
+        case "Martes":
+            echo "Hoy es martes.";
+            break;
+        case "Miércoles":
+            echo "Hoy es miércoles.";
+            break;
+        default:
+            echo "Hoy no es lunes, martes ni miércoles.";
+            break;
+    }
+    // Estructura de control de repetición "for":
+    for ($i = 1; $i <= 5; $i++) {
+        echo "El valor de i es: " . $i . "<br>";
+    }
+    // Estructura de control de repetición "while":
+    $num = 1;
+    
+    while ($num <= 10) {
+        echo $num . " ";
+        $num++;
+    }
+    // Estructura de control de repetición "do-while":
+    $num = 1;
+    
+    do {
+        echo $num . " ";
+        $num++;
+    } while ($num <= 10);
 ?>
 
