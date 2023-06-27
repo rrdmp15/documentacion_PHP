86aac77218991d0929ae49800197b5217b0e82b2
rrdmp15 <rdayismar@gmail.com>
2023-06-27 16:33:30 -0500

    feat: :sparkles: Variables y Constantes
    Agregado ejemplo de variables y constantes en PHP. Ilustra la declaración y uso de variables, interpolación de cadenas y cálculos con constantes. Contribuye a la comprensión de fundamentos de PHP y es útil como referencia educativa

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
-$mensaje = "Hola, mundo!";
+// Variable
+$nombre = "Juan";
+$edad = 25;
+echo "Mi nombre es $nombre y tengo $edad años.";
 
-echo $mensaje.'<br>';
-
-print($mensaje.'<br>');
+// Constante
+define("PI", 3.1416);
+$radio = 5;
+$area = PI * $radio * $radio;
+echo "El área de un círculo con radio $radio es: $area";
 ?>
